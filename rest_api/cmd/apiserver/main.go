166747b7470c935ac96831c1403d56f9600bc2d9
IKostarev <ilya.kostarev.9@gmail.com>
2023-03-28 18:10:25 +0500

    Добавил пакеты для конфига и логирования

--- rest_api/cmd/apiserver/main.go
+++ rest_api/cmd/apiserver/main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"rest_api/internal/api/apiserver"
+
+	"github.com/BurntSushi/toml"
 )
 
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "путь до файла конфига")
+}
+
 func main() {
-	s := apiserver.New()
+	flag.Parse()
+
+
+
+	cfg := apiserver.NewConfig()
+
+	_, err := toml.DecodeFile(configPath, cfg)
+	if err != nil {
+		log.Fatal("Ошибка при чтении файла конфига: ", err)
+	}
+
+	s := apiserver.New(cfg)
 	if err := s.Start(); err != nil {
 		log.Fatal("Ошибка при старте сервера: ", err)
 	}
-
-	
-
 }
